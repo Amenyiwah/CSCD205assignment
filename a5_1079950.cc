c4008b7baf00f7fefeb2ad83a302e5dd653f3c34
Amenyiwah <42740048+Amenyiwah@users.noreply.github.com>
2018-10-16 05:59:41 +0000

    Add files via upload

---
+++ a5_1079950.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+struct student{
+	int studentID[5];
+	int age[5];
+	string name[5];
+	char gender[5];
+	double score[5];
+	char grade[5];
+};
+
+int main()
+{
+	double total_scores=0;
+	int total_age=0;
+	int count_male;
+	int count_female;
+	double average_score=0;
+	double average_age=0;
+	int num_students;
+	num_students=5;
+	double my_score;
+	
+	
+	for(int i=o; i<num_students; i++)
+	{
+		cout<<"Please enter your ID :";
+		cin>>studentID[i];
+		
+		cout<<"\n Enter your Name :";
+		cin>>name[i];
+		
+		cout<<"\n Enter your age :";
+		cin>>age[i];
+		
+		cout<<"\n Enter your gender :";
+		cin>>gender[i];
+		
+		cout<<"Enter your score :";
+		cin>>score[i];
+		
+		if(score[i]>80&&score[i]<=100)
+		{
+			grade[i]="A";
+		}
+		else if(Dexoangle.score[i]="B")
+	}
+}
